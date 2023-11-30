605e060645f5df7eff5491e343648c7301c94075
DevJun <3636qq@naver.com>
2023-11-30 21:25:26 +0900

    crawling fin
    잡코리아 구인구직 데이터 크롤링 완료, csv 나옴
    잡플래닛 기업 리뷰 크롤링 스크립팅 완료, csv 제작 중

--- JaeJun/Crawling/JobKoreaJobDataCrawling.py
+++ JaeJun/Crawling/JobKoreaJobDataCrawling.py
@@ -12,26 +12,26 @@ page_no = 1
 url = f"https://www.jobkorea.co.kr/Search/?local=I000%2CB000%2CK000%2CG000%2C1000%2CO000%2CP000%2CE000%2CL000%2CM000%2CF000%2CD000%2CH000%2CJ000%2CC000%2CA000%2CN000%2CQ000&tabType=recruit&Page_No={page_no}" # 지역 "전체" 으로 검색한 쿼리문.
 
 
-url_0 = f"https://www.jobkorea.co.kr/Search/?local=I000&tabType=recruit&Page_No={page_no}"   # 지역 "서울" 으로 검색한 쿼리문.
-url_1 = f"https://www.jobkorea.co.kr/Search/?local=2CB000&tabType=recruit&Page_No={page_no}" # 지역 "경기" 으로 검색한 쿼리문.
-url_3 = f"https://www.jobkorea.co.kr/Search/?local=2CK000&tabType=recruit&Page_No={page_no}" # 지역 "인천" 으로 검색한 쿼리문.
-url_4 = f"https://www.jobkorea.co.kr/Search/?local=2CG000&tabType=recruit&Page_No={page_no}" # 지역 "대전" 으로 검색한 쿼리문.
-url_5 = f"https://www.jobkorea.co.kr/Search/?local=2C1000&tabType=recruit&Page_No={page_no}" # 지역 "세종" 으로 검색한 쿼리문.
-url_6 = f"https://www.jobkorea.co.kr/Search/?local=2CO000&tabType=recruit&Page_No={page_no}" # 지역 "충남" 으로 검색한 쿼리문.
-url_7 = f"https://www.jobkorea.co.kr/Search/?local=2CP000&tabType=recruit&Page_No={page_no}" # 지역 "충북" 으로 검색한 쿼리문.
-url_8 = f"https://www.jobkorea.co.kr/Search/?local=2CE000&tabType=recruit&Page_No={page_no}" # 지역 "광주" 으로 검색한 쿼리문.
-url_9 = f"https://www.jobkorea.co.kr/Search/?local=2CL000&tabType=recruit&Page_No={page_no}" # 지역 "전남" 으로 검색한 쿼리문.
-url_10 = f"https://www.jobkorea.co.kr/Search/?local=2CM000&tabType=recruit&Page_No={page_no}" # 지역 "전북" 으로 검색한 쿼리문.
-url_11 = f"https://www.jobkorea.co.kr/Search/?local=2CF000&tabType=recruit&Page_No={page_no}" # 지역 "대구" 으로 검색한 쿼리문.
-url_12 = f"https://www.jobkorea.co.kr/Search/?local=2CD000&tabType=recruit&Page_No={page_no}" # 지역 "경북" 으로 검색한 쿼리문.
-url_13 = f"https://www.jobkorea.co.kr/Search/?local=2CH000&tabType=recruit&Page_No={page_no}" # 지역 "부산" 으로 검색한 쿼리문.
-url_14 = f"https://www.jobkorea.co.kr/Search/?local=2CJ000&tabType=recruit&Page_No={page_no}" # 지역 "울산" 으로 검색한 쿼리문.
-url_15 = f"https://www.jobkorea.co.kr/Search/?local=2CC000&tabType=recruit&Page_No={page_no}" # 지역 "경남" 으로 검색한 쿼리문.
-url_16 = f"https://www.jobkorea.co.kr/Search/?local=2CA000&tabType=recruit&Page_No={page_no}" # 지역 "강원" 으로 검색한 쿼리문.
-url_17 = f"https://www.jobkorea.co.kr/Search/?local=2CN000&tabType=recruit&Page_No={page_no}" # 지역 "제주" 으로 검색한 쿼리문.
-url_18 = f"https://www.jobkorea.co.kr/Search/?local=2CQ000&tabType=recruit&Page_No={page_no}" # 지역 "전국" 으로 검색한 쿼리문.
-urls = [url_0 ,url_1 ,url_3 ,url_4 ,url_5 ,url_6 ,url_7 ,url_8 ,url_9 ,url_10,url_11,url_12,url_13,url_14,url_15,url_16,url_17,url_18]
-
+# url_0 = f"https://www.jobkorea.co.kr/Search/?local=I000&tabType=recruit&Page_No={page_no}"   # 지역 "서울" 으로 검색한 쿼리문.
+# url_1 = f"https://www.jobkorea.co.kr/Search/?local=2CB000&tabType=recruit&Page_No={page_no}" # 지역 "경기" 으로 검색한 쿼리문.
+# url_3 = f"https://www.jobkorea.co.kr/Search/?local=2CK000&tabType=recruit&Page_No={page_no}" # 지역 "인천" 으로 검색한 쿼리문.
+# url_4 = f"https://www.jobkorea.co.kr/Search/?local=2CG000&tabType=recruit&Page_No={page_no}" # 지역 "대전" 으로 검색한 쿼리문.
+# url_5 = f"https://www.jobkorea.co.kr/Search/?local=2C1000&tabType=recruit&Page_No={page_no}" # 지역 "세종" 으로 검색한 쿼리문.
+# url_6 = f"https://www.jobkorea.co.kr/Search/?local=2CO000&tabType=recruit&Page_No={page_no}" # 지역 "충남" 으로 검색한 쿼리문.
+# url_7 = f"https://www.jobkorea.co.kr/Search/?local=2CP000&tabType=recruit&Page_No={page_no}" # 지역 "충북" 으로 검색한 쿼리문.
+# url_8 = f"https://www.jobkorea.co.kr/Search/?local=2CE000&tabType=recruit&Page_No={page_no}" # 지역 "광주" 으로 검색한 쿼리문.
+# url_9 = f"https://www.jobkorea.co.kr/Search/?local=2CL000&tabType=recruit&Page_No={page_no}" # 지역 "전남" 으로 검색한 쿼리문.
+# url_10 = f"https://www.jobkorea.co.kr/Search/?local=2CM000&tabType=recruit&Page_No={page_no}" # 지역 "전북" 으로 검색한 쿼리문.
+# url_11 = f"https://www.jobkorea.co.kr/Search/?local=2CF000&tabType=recruit&Page_No={page_no}" # 지역 "대구" 으로 검색한 쿼리문.
+# url_12 = f"https://www.jobkorea.co.kr/Search/?local=2CD000&tabType=recruit&Page_No={page_no}" # 지역 "경북" 으로 검색한 쿼리문.
+# url_13 = f"https://www.jobkorea.co.kr/Search/?local=2CH000&tabType=recruit&Page_No={page_no}" # 지역 "부산" 으로 검색한 쿼리문.
+# url_14 = f"https://www.jobkorea.co.kr/Search/?local=2CJ000&tabType=recruit&Page_No={page_no}" # 지역 "울산" 으로 검색한 쿼리문.
+# url_15 = f"https://www.jobkorea.co.kr/Search/?local=2CC000&tabType=recruit&Page_No={page_no}" # 지역 "경남" 으로 검색한 쿼리문.
+# url_16 = f"https://www.jobkorea.co.kr/Search/?local=2CA000&tabType=recruit&Page_No={page_no}" # 지역 "강원" 으로 검색한 쿼리문.
+# url_17 = f"https://www.jobkorea.co.kr/Search/?local=2CN000&tabType=recruit&Page_No={page_no}" # 지역 "제주" 으로 검색한 쿼리문.
+# url_18 = f"https://www.jobkorea.co.kr/Search/?local=2CQ000&tabType=recruit&Page_No={page_no}" # 지역 "전국" 으로 검색한 쿼리문.
+# urls = [url_0 ,url_1 ,url_3 ,url_4 ,url_5 ,url_6 ,url_7 ,url_8 ,url_9 ,url_10,url_11,url_12,url_13,url_14,url_15,url_16,url_17,url_18]
+# urls.reverse()
 # DataFrame 선언
 df = pd.DataFrame( columns = ['회사명','공고명','채용 형태(경력, 신입)','학력','직장 위치','키워드','마감 기한','공고 링크'])
 
@@ -39,54 +39,54 @@ df = pd.DataFrame( columns = ['회사명','공고명','채용 형태(경력, 신
 # '총 OOOO건' 이라는 검색 결과를 스크래핑하여 한 페이지당 표시 수인 20으로 나누기
 
 URLindex = 0
-for url in urls: # 한번에 전체 지역을 돌릴 때 터질 위험을 위해 지역별로 분할
-    URLindex = URLindex + 1
+# for url in urls: # 한번에 전체 지역을 돌릴 때 터질 위험을 위해 지역별로 분할
+URLindex = URLindex + 1
+response = requests.get(url, headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'})
+response.encoding = 'cp949'
+soup = bs(response.text,'html.parser')
+
+pages = soup.find('p','filter-text').find('strong').text.replace(',','')
+pages = round(int(pages)/20)
+
+# For문을 이용해 웹 사이트 탐색
+# 바깥의 for문을 통해 페이지를 바꾸어가며 탐색하고
+# 안쪽의 for문으로 페이지 내 20개의 공고를 옮겨가며 탐색한다.
+
+for i in trange(pages):                       # for i in trange(pages): 를 통해 전체 페이지 탐색 가능. 시간이 오래걸려 일단 20페이지만 탐색
+
+    # url = f"https://www.jobkorea.co.kr/Search/?stext=%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%B6%84%EC%84%9D&tabType=recruit&Page_No={page_no}"
+
     response = requests.get(url, headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'})
-    response.encoding = 'cp949'
     soup = bs(response.text,'html.parser')
+    
+    company_name = soup.find_all('a','name dev_view')
+    exp = soup.find_all('span','exp')
+    edu = soup.find_all('span','edu')
+    loc = soup.find_all('span','loc long')
+    date = soup.find_all('span','date')
+    etc = soup.find_all('p','etc')
+    info = soup.find_all('a','title dev_view')
 
-    pages = soup.find('p','filter-text').find('strong').text.replace(',','')
-    pages = round(int(pages)/20)
+    for j in range(len(etc)):
+        try:
+            df.loc[20*i + j] = [
+            company_name[j].text,                                                   # 회사 이름
+            info[j].text.strip(),                                                   # 공고명
+            exp[j].text,                                                            # 채용 형태(경력, 신입)
+            edu[j].text,                                                            # 학력
+            loc[j].text,                                                            # 회사 위치
+            ','.join(etc[j].text.split(',')[:5]),                                   # 키워드
+            date[j].text,                                                           # 마감 기한
+            "https://www.jobkorea.co.kr/" + info[j]['href']                         # 공고 링크
+            ]
+            page_no += 1
 
-    # For문을 이용해 웹 사이트 탐색
-    # 바깥의 for문을 통해 페이지를 바꾸어가며 탐색하고
-    # 안쪽의 for문으로 페이지 내 20개의 공고를 옮겨가며 탐색한다.
+        except NameError as e:
+            print("Index Error", e.args)
+            page_no += 1
+    page_no += 1
+    time.sleep(0.1)
 
-    for i in trange(pages):                       # for i in trange(pages): 를 통해 전체 페이지 탐색 가능. 시간이 오래걸려 일단 20페이지만 탐색
-
-        # url = f"https://www.jobkorea.co.kr/Search/?stext=%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%B6%84%EC%84%9D&tabType=recruit&Page_No={page_no}"
-
-        response = requests.get(url, headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'})
-        soup = bs(response.text,'html.parser')
-        
-        company_name = soup.find_all('a','name dev_view')
-        exp = soup.find_all('span','exp')
-        edu = soup.find_all('span','edu')
-        loc = soup.find_all('span','loc long')
-        date = soup.find_all('span','date')
-        etc = soup.find_all('p','etc')
-        info = soup.find_all('a','title dev_view')
-
-        for j in range(len(etc)):
-            try:
-                df.loc[20*i + j] = [
-                company_name[j].text,                                                   # 회사 이름
-                info[j].text.strip(),                                                   # 공고명
-                exp[j].text,                                                            # 채용 형태(경력, 신입)
-                edu[j].text,                                                            # 학력
-                loc[j].text,                                                            # 회사 위치
-                ','.join(etc[j].text.split(',')[:5]),                                   # 키워드
-                date[j].text,                                                           # 마감 기한
-                "https://www.jobkorea.co.kr/" + info[j]['href']                         # 공고 링크
-                ]
-                page_no += 1
-
-            except NameError as e:
-                print("Index Error", e.args)
-                page_no += 1
-        page_no += 1
-        time.sleep(0.1)
-
-    file_name = str(datetime.date.today()) + "_" + URLindex + "_잡코리아_채용공고.csv"
-    df.to_csv(file_name,index=False, encoding="utf-8-sig")
-    pd.read_csv(file_name)
+file_name = str(datetime.date.today()) + "_" + str(URLindex) + "_잡코리아_채용공고.csv"
+df.to_csv(file_name,index=False, encoding="utf-8-sig")
+pd.read_csv(file_name)
--- JaeJun/Crawling/JobPlanetCompanyReviewCrawling.py
+++ JaeJun/Crawling/JobPlanetCompanyReviewCrawling.py
@@ -4,8 +4,17 @@ import pandas as pd
 import re
 import urllib3
 from tqdm import trange
+import time
+import datetime
+import argparse
+import os, sys, pickle
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-first',  type=int, default=1)
+parser.add_argument('-last',  type=int, default=2)
+args = parser.parse_args()
 
 login_url = 'https://www.jobplanet.co.kr/users/sign_in'
 
@@ -45,112 +54,132 @@ def clean_str(text):
 
 #uTl 은 보고싶은 기업의 리뷰 URL 이며 마지막은 ?page= 형태로 해야함, last_page는 해당 기업 리뷰의 마지막 페이지 입력 
 
-# soup = bs(response.text,'html.parser')
-
-# pages = soup.find('p','filter-text').find('strong').text.replace(',','')
-
 
 # last_page = 212
-last_page = 100 # 임시로 1
+# last_page = 10 # 임시로 1
 # lastCompanyIndex = 25833     # companyIndex : 1 ~ 25833
-lastCompanyIndex = 40000     # 임시로
+lastCompanyIndex = 10000     # 임시로
+reviewCountKeyword1 = '기업정보'
+reviewCountKeyword2 = '기업리뷰'
+reviewCountKeyword3 = '리뷰평점'
+reviewCount = 0
 
-for companyIndex in trange(1, lastCompanyIndex):
 
-    # url = 'https://www.jobplanet.co.kr/companies/' + str(companyIndex) + '? page='+str(1)
-    # res = session.get(url) 
-    # res.raise_for_status()
 
-    # soup = BeautifulSoup(res.text, 'html.parser')
+def CrawlCompanyReviewData(startCompanyCode, lastCompanyCode):
+    for companyIndex in range(startCompanyCode, lastCompanyCode):
 
-    # title = soup.find('h2', class_ = 'stats_ttl')
-    # # pages = title.find('h2')
-    # # print(pages.get_text().strip())
-    # # title = title.get_text()
-    # print(title)
+    # 전체 리뷰/5로 최대 페이지 수 구함
+        # url = 'https://www.jobplanet.co.kr/companies/' + str(companyIndex) + '? page='+str(1)
 
-    # exit()
-
-    # samsung = str(30139)
-    # print("")
-    # print("get soup from request....")
-
-    for idx in trange(1, last_page):
-        try:
-            # url = 'https://www.jobplanet.co.kr/companies/' + str(companyIndex) + '? page='+str(idx)
-            url = 'https://www.jobplanet.co.kr/companies/' + str(companyIndex) + '? page='+str(idx)
-            res = session.get(url) 
-            res.raise_for_status()
-
-            soup = BeautifulSoup(res.text, 'html.parser')
-
-            count3 = 0
-            count4 = 0
-            count5 = 0
-        except:
-            # print("No Company on id " + str(companyIndex) )
-            pass
-
-    # try:
-    for k in trange(5):  # 한페이지에 리뷰 5개씩 나오기 때문에 k=5
+        # try:    # 기업 코드가 있는 경우
         print("")
-        print("Flag")
-        reviewer_info = []
+        print("Company CODE : " + str(companyIndex))
+        url = 'https://www.jobplanet.co.kr/companies/' + str(companyIndex) + '? page='+str(1)
+        res = session.get(url) 
+        res.raise_for_status()
+        if res.status_code != 200:
+            continue
 
-        # 응답자 정보
-        position = soup.select('.content_top_ty2 > span.txt1') [0 + count4].text                # 직무
-        status   = soup.select('.content_top_ty2 > span.txt1') [1 + count4].text                # 상황
-        loc      = soup.select('.content_top_ty2 > span.txt1') [2 + count4].text                # 지역
+        else:
+            #print("res : " + str(res))
+            soup = BeautifulSoup(res.text, 'html.parser')
+            soup = soup.prettify() # 보기좋게 구분
+            fulltext = soup.split('\n')
+            status = ""
+            # print(fulltext)
+            for i in fulltext:
+                if reviewCountKeyword1 in i and reviewCountKeyword2 in i and reviewCountKeyword3 in i:
+                    print(i)
+                    status = i
+                    break
 
-        day      = soup.select('.content_top_ty2 > span.txt1') [3 + count4].text                # 작성일
+            status = status.split()
+            #print(len(status))
+            if len(status) == 0:
+                print("CASE : No Company")
+                continue
+            status[5] = status[5].strip('건,')
 
-        #점
-        star_rating = soup.select('.us_star_m > div.star_score') [0+k] ['style'] [6:-1]         # 총점
+            companyName = str(status[0])
+            if int(status[5]) <= 5:
+                print("CASE : No Review")
+                continue
 
-        # rating 5*5
-        promotion = soup.select('.bl_score') [0 + count5] ['style'][6:-1]                       # 승진 기회 및 가능성
-        welfare   = soup.select('.bl_score') [1 + count5] ['style'][6:-1]                       # 복지 및 급여
-        balance   = soup.select('.bl_score') [2 + count5] ['style'][6:-1]                       # 업무와 삶의 균형
-        culture   = soup.select('.bl_score') [3 + count5] ['style'][6:-1]                       # 사내 문화
-        top       = soup.select('.bl_score') [4 + count5] ['style'][6:-1]                       # 경영진
+            else :
+                print(int(status[5]))
+                #print("CASE : Riview exist")
+                reviewCount = int(status[5])
+                maxPageCount = int(reviewCount/5 + 1)
+                #print("maxPageCount : " + str(maxPageCount))
 
-        # 중심 제목
-        content = soup.select('h2.us_label')[0 + k].text                                        # 총평
-        # print("content : " + str(content))
-        # exit()
 
-        # 장단점 경영진 의견                                                                    
-        merit = soup.select('dl.tc_list > dd.df1 > span')[0 + count3].text                      # 장점
-        disadvantages = soup.select('dl.tc_list > dd.df1>span') [1 + count3].text               # 단점
-        df_tit = soup.select('dl.tc_list > dd.df1> span') [2 + count3].text                     # 바라는점                                                                               
-        
-        reviewer_info = [companyIndex , position, status, loc, day, star_rating, promotion, welfare, balance, culture,
-        top, clean_str(content), clean_str(merit), clean_str(disadvantages), clean_str(df_tit)]
-        # top, content, merit, disadvantages, df_tit]
+            for idx in trange(1, maxPageCount):
+                try:
+                    # url = 'https://www.jobplanet.co.kr/companies/' + str(companyIndex) + '? page='+str(idx)
+                    url = 'https://www.jobplanet.co.kr/companies/' + str(companyIndex) + '? page='+str(idx)
+                    res = session.get(url) 
+                    res.raise_for_status()
 
-        result.append(reviewer_info)
-        reviewer_info=[]
-        count3 += 3
-        count4 += 4
-        count5 += 5
-        print("pass :"+str(idx)+"-"+str(k))
-        print("result : " + str(result))
-    # except :
+                    soup = BeautifulSoup(res.text, 'html.parser')
+                    count3 = 0
+                    count4 = 0
+                    count5 = 0
+                except:
+                    #print("No Company on id " + str(companyIndex) )
+                    print("CASE : out")
+                    continue
 
-    # print("@@@")
-    # print("fail" + str(idx))
-    # pass
+            # try:
+                for k in range(5):  # 한페이지에 리뷰 5개씩 나오기 때문에 k=5
+                    reviewer_info = []
+
+                    # 응답자 정보
+                    position = soup.select('.content_top_ty2 > span.txt1') [0 + count4].text                # 직무
+                    status   = soup.select('.content_top_ty2 > span.txt1') [1 + count4].text                # 상황
+                    loc      = soup.select('.content_top_ty2 > span.txt1') [2 + count4].text                # 지역
+                    day      = soup.select('.content_top_ty2 > span.txt1') [3 + count4].text                # 작성일
+
+                    #점
+                    star_rating = soup.select('.us_star_m > div.star_score') [0+k] ['style'] [6:-1]         # 총점
+
+                    # rating 5*5
+                    promotion = soup.select('.bl_score') [0 + count5] ['style'][6:-1]                       # 승진 기회 및 가능성
+                    welfare   = soup.select('.bl_score') [1 + count5] ['style'][6:-1]                       # 복지 및 급여
+                    balance   = soup.select('.bl_score') [2 + count5] ['style'][6:-1]                       # 업무와 삶의 균형
+                    culture   = soup.select('.bl_score') [3 + count5] ['style'][6:-1]                       # 사내 문화
+                    top       = soup.select('.bl_score') [4 + count5] ['style'][6:-1]                       # 경영진
+
+                    # 중심 제목
+                    content = soup.select('h2.us_label')[0 + k].text                                        # 총평
+
+                    # 장단점 경영진 의견                                                                    
+                    merit = soup.select('dl.tc_list > dd.df1 > span')[0 + count3].text                      # 장점
+                    disadvantages = soup.select('dl.tc_list > dd.df1>span') [1 + count3].text               # 단점
+                    df_tit = soup.select('dl.tc_list > dd.df1> span') [2 + count3].text                     # 바라는점                                                                               
+                    
+                    reviewer_info = [companyName , position, status, loc, day, star_rating, promotion, welfare, balance, culture,
+                    top, content, merit, disadvantages, df_tit]
+                    # top, clean_str(content), clean_str(merit), clean_str(disadvantages), clean_str(df_tit)]
+
+                    result.append(reviewer_info)
+                    reviewer_info=[]
+                    count3 += 3
+                    count4 += 4
+                    count5 += 5
+            # except :
+
 
     colname = ['회사명', '직무','상황', '지역', '작성일', '총점', '승진 기회 및 가능성', '복지 및 급여', '업무와 삶의 균형', '사내문화', '경영진', '총평', '장점', '단점', '바라는점']
     df = pd.DataFrame(result, columns=colname)
-    print("")
 
-#저장을 희망하는 파일명으로 저장
-file_name = "jobPlanet_companyReview.csv"
-df.to_csv(file_name,index=False, encoding="utf-8-sig")
-
+    #저장을 희망하는 파일명으로 저장
+    file_name = str(datetime.date.today())+"_" + str(startCompanyCode) + "_" +"jobPlanet_companyReview2.csv"
+    df.to_csv(file_name,index=False, encoding="utf-8-sig")
 
 
 
 
-print(result)
+if __name__ == "__main__":
+    argv = sys.argv
+    CrawlCompanyReviewData(args.first, args.last)
